079a7bbfbe40eb38704eaeab72047fd817eecab8
TCN CI <ci@tcnp3.com>
2023-07-31 18:09:21 +0000

    update tcnapi-jdk8 > added call_length to commons auto_evaluation message

--- src/main/java/com/tcn/cloud/api/api/commons/ScorecardOrBuilder.java
+++ src/main/java/com/tcn/cloud/api/api/commons/ScorecardOrBuilder.java
@@ -252,7 +252,7 @@ public interface ScorecardOrBuilder extends
    *
    * <code>bool is_ad_hoc = 16 [json_name = "isAdHoc", deprecated = true];</code>
    * @deprecated api.commons.Scorecard.is_ad_hoc is deprecated.
-   *     See api/commons/scorecards.proto;l=289
+   *     See api/commons/scorecards.proto;l=290
    * @return The isAdHoc.
    */
   @java.lang.Deprecated boolean getIsAdHoc();
