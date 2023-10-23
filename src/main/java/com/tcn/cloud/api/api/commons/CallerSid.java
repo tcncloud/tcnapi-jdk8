18f0fb988c16d4e3a4d533962bc671bdc4b54aa9
TCN CI <ci@tcnp3.com>
2023-10-23 16:38:03 +0000

    update tcnapi-jdk8 > Merge branch '226-room303-supervisor-and-member-permissions' into 'master'
    Resolve "Room303 - Supervisor and Member permissions"
    
    Closes #226
    
    See merge request tcnpublic/api!250

--- src/main/java/com/tcn/cloud/api/api/commons/CallerSid.java
+++ src/main/java/com/tcn/cloud/api/api/commons/CallerSid.java
@@ -21,7 +21,6 @@ private static final long serialVersionUID = 0L;
   }
   private CallerSid() {
     type_ = 0;
-    orgId_ = "";
   }
 
   @java.lang.Override
@@ -100,53 +99,6 @@ private static final long serialVersionUID = 0L;
     return tenantSid_;
   }
 
-  public static final int ORG_ID_FIELD_NUMBER = 4;
-  @SuppressWarnings("serial")
-  private volatile java.lang.Object orgId_ = "";
-  /**
-   * <pre>
-   * org id of the caller
-   * </pre>
-   *
-   * <code>string org_id = 4 [json_name = "orgId"];</code>
-   * @return The orgId.
-   */
-  @java.lang.Override
-  public java.lang.String getOrgId() {
-    java.lang.Object ref = orgId_;
-    if (ref instanceof java.lang.String) {
-      return (java.lang.String) ref;
-    } else {
-      com.google.protobuf.ByteString bs = 
-          (com.google.protobuf.ByteString) ref;
-      java.lang.String s = bs.toStringUtf8();
-      orgId_ = s;
-      return s;
-    }
-  }
-  /**
-   * <pre>
-   * org id of the caller
-   * </pre>
-   *
-   * <code>string org_id = 4 [json_name = "orgId"];</code>
-   * @return The bytes for orgId.
-   */
-  @java.lang.Override
-  public com.google.protobuf.ByteString
-      getOrgIdBytes() {
-    java.lang.Object ref = orgId_;
-    if (ref instanceof java.lang.String) {
-      com.google.protobuf.ByteString b = 
-          com.google.protobuf.ByteString.copyFromUtf8(
-              (java.lang.String) ref);
-      orgId_ = b;
-      return b;
-    } else {
-      return (com.google.protobuf.ByteString) ref;
-    }
-  }
-
   private byte memoizedIsInitialized = -1;
   @java.lang.Override
   public final boolean isInitialized() {
@@ -170,9 +122,6 @@ private static final long serialVersionUID = 0L;
     if (tenantSid_ != 0L) {
       output.writeInt64(3, tenantSid_);
     }
-    if (!com.google.protobuf.GeneratedMessageV3.isStringEmpty(orgId_)) {
-      com.google.protobuf.GeneratedMessageV3.writeString(output, 4, orgId_);
-    }
     getUnknownFields().writeTo(output);
   }
 
@@ -194,9 +143,6 @@ private static final long serialVersionUID = 0L;
       size += com.google.protobuf.CodedOutputStream
         .computeInt64Size(3, tenantSid_);
     }
-    if (!com.google.protobuf.GeneratedMessageV3.isStringEmpty(orgId_)) {
-      size += com.google.protobuf.GeneratedMessageV3.computeStringSize(4, orgId_);
-    }
     size += getUnknownFields().getSerializedSize();
     memoizedSize = size;
     return size;
@@ -217,8 +163,6 @@ private static final long serialVersionUID = 0L;
     if (type_ != other.type_) return false;
     if (getTenantSid()
         != other.getTenantSid()) return false;
-    if (!getOrgId()
-        .equals(other.getOrgId())) return false;
     if (!getUnknownFields().equals(other.getUnknownFields())) return false;
     return true;
   }
@@ -238,8 +182,6 @@ private static final long serialVersionUID = 0L;
     hash = (37 * hash) + TENANT_SID_FIELD_NUMBER;
     hash = (53 * hash) + com.google.protobuf.Internal.hashLong(
         getTenantSid());
-    hash = (37 * hash) + ORG_ID_FIELD_NUMBER;
-    hash = (53 * hash) + getOrgId().hashCode();
     hash = (29 * hash) + getUnknownFields().hashCode();
     memoizedHashCode = hash;
     return hash;
@@ -378,7 +320,6 @@ private static final long serialVersionUID = 0L;
       callerSid_ = 0L;
       type_ = 0;
       tenantSid_ = 0L;
-      orgId_ = "";
       return this;
     }
 
@@ -420,9 +361,6 @@ private static final long serialVersionUID = 0L;
       }
       if (((from_bitField0_ & 0x00000004) != 0)) {
         result.tenantSid_ = tenantSid_;
-      }
-      if (((from_bitField0_ & 0x00000008) != 0)) {
-        result.orgId_ = orgId_;
       }
     }
 
@@ -479,11 +417,6 @@ private static final long serialVersionUID = 0L;
       if (other.getTenantSid() != 0L) {
         setTenantSid(other.getTenantSid());
       }
-      if (!other.getOrgId().isEmpty()) {
-        orgId_ = other.orgId_;
-        bitField0_ |= 0x00000008;
-        onChanged();
-      }
       this.mergeUnknownFields(other.getUnknownFields());
       onChanged();
       return this;
@@ -525,11 +458,6 @@ private static final long serialVersionUID = 0L;
               bitField0_ |= 0x00000004;
               break;
             } // case 24
-            case 34: {
-              orgId_ = input.readStringRequireUtf8();
-              bitField0_ |= 0x00000008;
-              break;
-            } // case 34
             default: {
               if (!super.parseUnknownField(input, extensionRegistry, tag)) {
                 done = true; // was an endgroup tag
@@ -704,98 +632,6 @@ private static final long serialVersionUID = 0L;
     public Builder clearTenantSid() {
       bitField0_ = (bitField0_ & ~0x00000004);
       tenantSid_ = 0L;
-      onChanged();
-      return this;
-    }
-
-    private java.lang.Object orgId_ = "";
-    /**
-     * <pre>
-     * org id of the caller
-     * </pre>
-     *
-     * <code>string org_id = 4 [json_name = "orgId"];</code>
-     * @return The orgId.
-     */
-    public java.lang.String getOrgId() {
-      java.lang.Object ref = orgId_;
-      if (!(ref instanceof java.lang.String)) {
-        com.google.protobuf.ByteString bs =
-            (com.google.protobuf.ByteString) ref;
-        java.lang.String s = bs.toStringUtf8();
-        orgId_ = s;
-        return s;
-      } else {
-        return (java.lang.String) ref;
-      }
-    }
-    /**
-     * <pre>
-     * org id of the caller
-     * </pre>
-     *
-     * <code>string org_id = 4 [json_name = "orgId"];</code>
-     * @return The bytes for orgId.
-     */
-    public com.google.protobuf.ByteString
-        getOrgIdBytes() {
-      java.lang.Object ref = orgId_;
-      if (ref instanceof String) {
-        com.google.protobuf.ByteString b = 
-            com.google.protobuf.ByteString.copyFromUtf8(
-                (java.lang.String) ref);
-        orgId_ = b;
-        return b;
-      } else {
-        return (com.google.protobuf.ByteString) ref;
-      }
-    }
-    /**
-     * <pre>
-     * org id of the caller
-     * </pre>
-     *
-     * <code>string org_id = 4 [json_name = "orgId"];</code>
-     * @param value The orgId to set.
-     * @return This builder for chaining.
-     */
-    public Builder setOrgId(
-        java.lang.String value) {
-      if (value == null) { throw new NullPointerException(); }
-      orgId_ = value;
-      bitField0_ |= 0x00000008;
-      onChanged();
-      return this;
-    }
-    /**
-     * <pre>
-     * org id of the caller
-     * </pre>
-     *
-     * <code>string org_id = 4 [json_name = "orgId"];</code>
-     * @return This builder for chaining.
-     */
-    public Builder clearOrgId() {
-      orgId_ = getDefaultInstance().getOrgId();
-      bitField0_ = (bitField0_ & ~0x00000008);
-      onChanged();
-      return this;
-    }
-    /**
-     * <pre>
-     * org id of the caller
-     * </pre>
-     *
-     * <code>string org_id = 4 [json_name = "orgId"];</code>
-     * @param value The bytes for orgId to set.
-     * @return This builder for chaining.
-     */
-    public Builder setOrgIdBytes(
-        com.google.protobuf.ByteString value) {
-      if (value == null) { throw new NullPointerException(); }
-      checkByteStringIsUtf8(value);
-      orgId_ = value;
-      bitField0_ |= 0x00000008;
       onChanged();
       return this;
     }
