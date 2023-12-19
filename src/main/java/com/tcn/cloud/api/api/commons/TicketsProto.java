394c45e24f7305382b30ef5c5acf6306fcbebf68
TCN CI <ci@tcnp3.com>
2023-12-19 21:40:39 +0000

    update tcnapi-jdk8 > Merge branch '309-create-proxy-for-phone-number-validation-2' into 'master'
    Resolve "Create Proxy For Phone Number Validation"
    
    Closes #309
    
    See merge request tcnpublic/api!333

--- src/main/java/com/tcn/cloud/api/api/commons/TicketsProto.java
+++ src/main/java/com/tcn/cloud/api/api/commons/TicketsProto.java
@@ -312,10 +312,12 @@ public final class TicketsProto {
       "TIME_SCALE_WEEK\020\003\022\024\n\020TIME_SCALE_MONTH\020\004\022" +
       "\023\n\017TIME_SCALE_YEAR\020\005*V\n\014TicketStatus\022\025\n\021" +
       "TICKET_STATUS_NEW\020\000\022\026\n\022TICKET_STATUS_OPE" +
-      "N\020\001\022\027\n\023TICKET_STATUS_CLOSE\020\002Bz\n\035com.tcn." +
-      "cloud.api.api.commonsB\014TicketsProtoP\001\242\002\003" +
-      "ACX\252\002\013Api.Commons\312\002\013Api\\Commons\342\002\027Api\\Co" +
-      "mmons\\GPBMetadata\352\002\014Api::Commonsb\006proto3"
+      "N\020\001\022\027\n\023TICKET_STATUS_CLOSE\020\002*;\n\017PhoneNum" +
+      "berType\022\n\n\006MOBILE\020\000\022\t\n\005OTHER\020\001\022\021\n\rINVALI" +
+      "D_ERROR\020\002Bz\n\035com.tcn.cloud.api.api.commo" +
+      "nsB\014TicketsProtoP\001\242\002\003ACX\252\002\013Api.Commons\312\002" +
+      "\013Api\\Commons\342\002\027Api\\Commons\\GPBMetadata\352\002" +
+      "\014Api::Commonsb\006proto3"
     };
     descriptor = com.google.protobuf.Descriptors.FileDescriptor
       .internalBuildGeneratedFileFrom(descriptorData,
--- src/main/java/com/tcn/cloud/api/api/v1alpha1/tickets/ServiceProto.java
+++ src/main/java/com/tcn/cloud/api/api/v1alpha1/tickets/ServiceProto.java
@@ -27,7 +27,7 @@ public final class ServiceProto {
       "i.v1alpha1.tickets\032\027annotations/authz.pr" +
       "oto\032\"api/v1alpha1/tickets/project.proto\032" +
       "!api/v1alpha1/tickets/ticket.proto\032\034goog" +
-      "le/api/annotations.proto2\345(\n\007Tickets\022\235\001\n" +
+      "le/api/annotations.proto2\253*\n\007Tickets\022\235\001\n" +
       "\014CreateTicket\022%.api.v1alpha1.tickets.Cre" +
       "ateTicketReq\032%.api.v1alpha1.tickets.Crea" +
       "teTicketRes\"?\272\270\221\002\005\n\003\010\234\030\202\323\344\223\002/\"*/api/v1al" +
@@ -158,11 +158,16 @@ public final class ServiceProto {
       "peRequest\032+.api.v1alpha1.tickets.GetActi" +
       "onTypeResponse\"H\272\270\221\002\n\n\003\010\235\030\n\003\010\234\030\202\323\344\223\0023\"./" +
       "api/v1alpha1/tickets/tickets/getallActio" +
-      "ntype:\001*B\250\001\n&com.tcn.cloud.api.api.v1alp" +
-      "ha1.ticketsB\014ServiceProtoP\001\242\002\003AVT\252\002\024Api." +
-      "V1alpha1.Tickets\312\002\024Api\\V1alpha1\\Tickets\342" +
-      "\002 Api\\V1alpha1\\Tickets\\GPBMetadata\352\002\026Api" +
-      "::V1alpha1::Ticketsb\006proto3"
+      "ntype:\001*\022\303\001\n\022GetPhoneNumberType\022/.api.v1" +
+      "alpha1.tickets.GetPhoneNumberTypeRequest" +
+      "\0320.api.v1alpha1.tickets.GetPhoneNumberTy" +
+      "peResponse\"J\272\270\221\002\n\n\003\010\235\030\n\003\010\234\030\202\323\344\223\0025\"0/api/" +
+      "v1alpha1/tickets/tickets/getphonenumbert" +
+      "ype:\001*B\250\001\n&com.tcn.cloud.api.api.v1alpha" +
+      "1.ticketsB\014ServiceProtoP\001\242\002\003AVT\252\002\024Api.V1" +
+      "alpha1.Tickets\312\002\024Api\\V1alpha1\\Tickets\342\002 " +
+      "Api\\V1alpha1\\Tickets\\GPBMetadata\352\002\026Api::" +
+      "V1alpha1::Ticketsb\006proto3"
     };
     descriptor = com.google.protobuf.Descriptors.FileDescriptor
       .internalBuildGeneratedFileFrom(descriptorData,
