e49018d01025478fbe61256e10ced96e605d59c4
TCN CI <ci@tcnp3.com>
2024-02-14 21:37:08 +0000

    update tcnapi-jdk8 > TCN Public API #382.  Business Hours plus Create.

--- src/main/java/com/tcn/cloud/api/api/commons/org/PreferencesProto.java
+++ src/main/java/com/tcn/cloud/api/api/commons/org/PreferencesProto.java
@@ -234,6 +234,26 @@ public final class PreferencesProto {
   static final 
     com.google.protobuf.GeneratedMessageV3.FieldAccessorTable
       internal_static_api_commons_org_AdminClientPreferences_fieldAccessorTable;
+  static final com.google.protobuf.Descriptors.Descriptor
+    internal_static_api_commons_org_BusinessHours_descriptor;
+  static final 
+    com.google.protobuf.GeneratedMessageV3.FieldAccessorTable
+      internal_static_api_commons_org_BusinessHours_fieldAccessorTable;
+  static final com.google.protobuf.Descriptors.Descriptor
+    internal_static_api_commons_org_Range_descriptor;
+  static final 
+    com.google.protobuf.GeneratedMessageV3.FieldAccessorTable
+      internal_static_api_commons_org_Range_fieldAccessorTable;
+  static final com.google.protobuf.Descriptors.Descriptor
+    internal_static_api_commons_org_CreateBusinessHoursRequest_descriptor;
+  static final 
+    com.google.protobuf.GeneratedMessageV3.FieldAccessorTable
+      internal_static_api_commons_org_CreateBusinessHoursRequest_fieldAccessorTable;
+  static final com.google.protobuf.Descriptors.Descriptor
+    internal_static_api_commons_org_CreateBusinessHoursResponse_descriptor;
+  static final 
+    com.google.protobuf.GeneratedMessageV3.FieldAccessorTable
+      internal_static_api_commons_org_CreateBusinessHoursResponse_fieldAccessorTable;
 
   public static com.google.protobuf.Descriptors.FileDescriptor
       getDescriptor() {
@@ -547,11 +567,22 @@ public final class PreferencesProto {
       "e_id\030\021 \001(\tR\022callbacksServiceId\0224\n\026agent_" +
       "screen_recording\030\022 \001(\010R\024agentScreenRecor" +
       "ding\022A\n\021allowed_countries\030\023 \003(\0162\024.api.co" +
-      "mmons.CountryR\020allowedCountriesB\223\001\n!com." +
-      "tcn.cloud.api.api.commons.orgB\020Preferenc" +
-      "esProtoP\001\242\002\003ACO\252\002\017Api.Commons.Org\312\002\017Api\\" +
-      "Commons\\Org\342\002\033Api\\Commons\\Org\\GPBMetadat" +
-      "a\352\002\021Api::Commons::Orgb\006proto3"
+      "mmons.CountryR\020allowedCountries\"\234\001\n\rBusi" +
+      "nessHours\022\025\n\006org_id\030\001 \001(\tR\005orgId\022\016\n\002id\030\002" +
+      " \001(\tR\002id\022\022\n\004name\030\003 \001(\tR\004name\022 \n\013descript" +
+      "ion\030\004 \001(\tR\013description\022.\n\006ranges\030\005 \003(\0132\026" +
+      ".api.commons.org.RangeR\006ranges\"\203\001\n\005Range" +
+      "\022\035\n\nstart_hour\030\001 \001(\005R\tstartHour\022!\n\014start" +
+      "_minute\030\002 \001(\005R\013startMinute\022\031\n\010end_hour\030\003" +
+      " \001(\005R\007endHour\022\035\n\nend_minute\030\004 \001(\005R\tendMi" +
+      "nute\"z\n\032CreateBusinessHoursRequest\022\025\n\006or" +
+      "g_id\030\001 \001(\tR\005orgId\022E\n\016business_hours\030\002 \001(" +
+      "\0132\036.api.commons.org.BusinessHoursR\rbusin" +
+      "essHours\"\035\n\033CreateBusinessHoursResponseB" +
+      "\223\001\n!com.tcn.cloud.api.api.commons.orgB\020P" +
+      "referencesProtoP\001\242\002\003ACO\252\002\017Api.Commons.Or" +
+      "g\312\002\017Api\\Commons\\Org\342\002\033Api\\Commons\\Org\\GP" +
+      "BMetadata\352\002\021Api::Commons::Orgb\006proto3"
     };
     descriptor = com.google.protobuf.Descriptors.FileDescriptor
       .internalBuildGeneratedFileFrom(descriptorData,
@@ -826,6 +857,30 @@ public final class PreferencesProto {
       com.google.protobuf.GeneratedMessageV3.FieldAccessorTable(
         internal_static_api_commons_org_AdminClientPreferences_descriptor,
         new java.lang.String[] { "OrgId", "UseReservedCarrier", "ReservedCarriers", "EmailKey", "EmailId", "EmailName", "WhitelistIps", "WhitelistDomains", "CallbacksServiceId", "AgentScreenRecording", "AllowedCountries", });
+    internal_static_api_commons_org_BusinessHours_descriptor =
+      getDescriptor().getMessageTypes().get(38);
+    internal_static_api_commons_org_BusinessHours_fieldAccessorTable = new
+      com.google.protobuf.GeneratedMessageV3.FieldAccessorTable(
+        internal_static_api_commons_org_BusinessHours_descriptor,
+        new java.lang.String[] { "OrgId", "Id", "Name", "Description", "Ranges", });
+    internal_static_api_commons_org_Range_descriptor =
+      getDescriptor().getMessageTypes().get(39);
+    internal_static_api_commons_org_Range_fieldAccessorTable = new
+      com.google.protobuf.GeneratedMessageV3.FieldAccessorTable(
+        internal_static_api_commons_org_Range_descriptor,
+        new java.lang.String[] { "StartHour", "StartMinute", "EndHour", "EndMinute", });
+    internal_static_api_commons_org_CreateBusinessHoursRequest_descriptor =
+      getDescriptor().getMessageTypes().get(40);
+    internal_static_api_commons_org_CreateBusinessHoursRequest_fieldAccessorTable = new
+      com.google.protobuf.GeneratedMessageV3.FieldAccessorTable(
+        internal_static_api_commons_org_CreateBusinessHoursRequest_descriptor,
+        new java.lang.String[] { "OrgId", "BusinessHours", });
+    internal_static_api_commons_org_CreateBusinessHoursResponse_descriptor =
+      getDescriptor().getMessageTypes().get(41);
+    internal_static_api_commons_org_CreateBusinessHoursResponse_fieldAccessorTable = new
+      com.google.protobuf.GeneratedMessageV3.FieldAccessorTable(
+        internal_static_api_commons_org_CreateBusinessHoursResponse_descriptor,
+        new java.lang.String[] { });
     com.tcn.cloud.api.api.commons.AnaProto.getDescriptor();
     com.tcn.cloud.api.api.commons.CountryProto.getDescriptor();
     com.tcn.cloud.api.api.commons.LmsProto.getDescriptor();
