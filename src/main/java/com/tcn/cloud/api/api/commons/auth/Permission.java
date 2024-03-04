65862f6ac3309fc512c0677d4585ea2031845ec4
TCN CI <ci@tcnp3.com>
2024-03-04 19:58:35 +0000

    update tcnapi-jdk8 > Merge branch '401-omni-asm-api' into 'master'
    Resolve "OMNI ASM API"
    
    Closes #401
    
    See merge request tcnpublic/api!422

--- src/main/java/com/tcn/cloud/api/api/commons/auth/Permission.java
+++ src/main/java/com/tcn/cloud/api/api/commons/auth/Permission.java
@@ -440,9 +440,8 @@ public enum Permission
    * deprecated
    * </pre>
    *
-   * <code>PERMISSION_INSIGHTS_COMMON_LIBRARY_MANAGE = 603 [deprecated = true];</code>
+   * <code>PERMISSION_INSIGHTS_COMMON_LIBRARY_MANAGE = 603 [(.annotations.perms.options) = { ... }</code>
    */
-  @java.lang.Deprecated
   PERMISSION_INSIGHTS_COMMON_LIBRARY_MANAGE(603),
   /**
    * <pre>
@@ -450,9 +449,8 @@ public enum Permission
    * deprecated
    * </pre>
    *
-   * <code>PERMISSION_INSIGHTS_MANAGE = 604 [deprecated = true];</code>
+   * <code>PERMISSION_INSIGHTS_MANAGE = 604 [(.annotations.perms.options) = { ... }</code>
    */
-  @java.lang.Deprecated
   PERMISSION_INSIGHTS_MANAGE(604),
   /**
    * <pre>
@@ -476,9 +474,8 @@ public enum Permission
    * deprecated
    * </pre>
    *
-   * <code>PERMISSION_INSIGHTS_DASHBOARD_VIEW = 607 [deprecated = true];</code>
+   * <code>PERMISSION_INSIGHTS_DASHBOARD_VIEW = 607 [(.annotations.perms.options) = { ... }</code>
    */
-  @java.lang.Deprecated
   PERMISSION_INSIGHTS_DASHBOARD_VIEW(607),
   /**
    * <pre>
@@ -486,9 +483,8 @@ public enum Permission
    * deprecated
    * </pre>
    *
-   * <code>PERMISSION_INSIGHTS_DASHBOARD_EDIT = 608 [deprecated = true];</code>
+   * <code>PERMISSION_INSIGHTS_DASHBOARD_EDIT = 608 [(.annotations.perms.options) = { ... }</code>
    */
-  @java.lang.Deprecated
   PERMISSION_INSIGHTS_DASHBOARD_EDIT(608),
   /**
    * <pre>
@@ -1222,18 +1218,18 @@ public enum Permission
    * deprecated
    * </pre>
    *
-   * <code>PERMISSION_INSIGHTS_COMMON_LIBRARY_MANAGE = 603 [deprecated = true];</code>
+   * <code>PERMISSION_INSIGHTS_COMMON_LIBRARY_MANAGE = 603 [(.annotations.perms.options) = { ... }</code>
    */
-  @java.lang.Deprecated public static final int PERMISSION_INSIGHTS_COMMON_LIBRARY_MANAGE_VALUE = 603;
+  public static final int PERMISSION_INSIGHTS_COMMON_LIBRARY_MANAGE_VALUE = 603;
   /**
    * <pre>
    * Enables access to insights.
    * deprecated
    * </pre>
    *
-   * <code>PERMISSION_INSIGHTS_MANAGE = 604 [deprecated = true];</code>
+   * <code>PERMISSION_INSIGHTS_MANAGE = 604 [(.annotations.perms.options) = { ... }</code>
    */
-  @java.lang.Deprecated public static final int PERMISSION_INSIGHTS_MANAGE_VALUE = 604;
+  public static final int PERMISSION_INSIGHTS_MANAGE_VALUE = 604;
   /**
    * <pre>
    * allows access to read insights
@@ -1256,18 +1252,18 @@ public enum Permission
    * deprecated
    * </pre>
    *
-   * <code>PERMISSION_INSIGHTS_DASHBOARD_VIEW = 607 [deprecated = true];</code>
+   * <code>PERMISSION_INSIGHTS_DASHBOARD_VIEW = 607 [(.annotations.perms.options) = { ... }</code>
    */
-  @java.lang.Deprecated public static final int PERMISSION_INSIGHTS_DASHBOARD_VIEW_VALUE = 607;
+  public static final int PERMISSION_INSIGHTS_DASHBOARD_VIEW_VALUE = 607;
   /**
    * <pre>
    * allows user to edit insights for dashboards
    * deprecated
    * </pre>
    *
-   * <code>PERMISSION_INSIGHTS_DASHBOARD_EDIT = 608 [deprecated = true];</code>
+   * <code>PERMISSION_INSIGHTS_DASHBOARD_EDIT = 608 [(.annotations.perms.options) = { ... }</code>
    */
-  @java.lang.Deprecated public static final int PERMISSION_INSIGHTS_DASHBOARD_EDIT_VALUE = 608;
+  public static final int PERMISSION_INSIGHTS_DASHBOARD_EDIT_VALUE = 608;
   /**
    * <pre>
    * Enables access to room 303 chat at a base level
