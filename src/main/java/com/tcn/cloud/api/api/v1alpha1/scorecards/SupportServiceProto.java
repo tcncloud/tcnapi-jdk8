e1d4c3e87995137d191b4540b07f86abd7317f86
TCN CI <ci@tcnp3.com>
2024-04-11 18:46:36 +0000

    update tcnapi-jdk8 > follow existing pattern

--- src/main/java/com/tcn/cloud/api/api/v1alpha1/scorecards/SupportServiceProto.java
+++ src/main/java/com/tcn/cloud/api/api/v1alpha1/scorecards/SupportServiceProto.java
@@ -29,41 +29,41 @@ public final class SupportServiceProto {
       "ds/auto_evaluation.proto\032(api/v1alpha1/s" +
       "corecards/evaluation.proto\032\'api/v1alpha1" +
       "/scorecards/scorecard.proto\032\034google/api/" +
-      "annotations.proto2\215\t\n\021ScorecardsSupport\022" +
-      "\333\001\n\026ListEvaluationsByOrgId\0226.api.v1alpha" +
+      "annotations.proto2\210\t\n\021ScorecardsSupport\022" +
+      "\332\001\n\026ListEvaluationsByOrgId\0226.api.v1alpha" +
       "1.scorecards.ListEvaluationsByOrgIdReque" +
       "st\0320.api.v1alpha1.scorecards.ListEvaluat" +
-      "ionsResponse\"W\272\270\221\002\005\n\003\010\310\001\202\323\344\223\002G\"B/api/v1a" +
-      "lpha1/scorecards/scorecards_support/list" +
-      "evaluationsbyorgid:\001*\022\353\001\n\032ListAutoEvalua" +
-      "tionsByOrgId\022:.api.v1alpha1.scorecards.L" +
-      "istAutoEvaluationsByOrgIdRequest\0324.api.v" +
-      "1alpha1.scorecards.ListAutoEvaluationsRe" +
-      "sponse\"[\272\270\221\002\005\n\003\010\310\001\202\323\344\223\002K\"F/api/v1alpha1/" +
-      "scorecards/scorecards_support/listautoev" +
-      "aluationsbyorgid:\001*\022\337\001\n\027DeleteEvaluation" +
-      "ByOrgId\0227.api.v1alpha1.scorecards.Delete" +
-      "EvaluationByOrgIdRequest\0321.api.v1alpha1." +
-      "scorecards.DeleteEvaluationResponse\"X\272\270\221" +
-      "\002\005\n\003\010\310\001\202\323\344\223\002H\"C/api/v1alpha1/scorecards/" +
-      "scorecards_support/deleteevaluationbyorg" +
-      "id:\001*\022\357\001\n\033DeleteAutoEvaluationByOrgId\022;." +
-      "api.v1alpha1.scorecards.DeleteAutoEvalua" +
-      "tionByOrgIdRequest\0325.api.v1alpha1.scorec" +
-      "ards.DeleteAutoEvaluationResponse\"\\\272\270\221\002\005" +
-      "\n\003\010\310\001\202\323\344\223\002L\"G/api/v1alpha1/scorecards/sc" +
-      "orecards_support/deleteautoevaluationbyo" +
-      "rgid:\001*\022\327\001\n\025ListScorecardsByOrgId\0225.api." +
-      "v1alpha1.scorecards.ListScorecardsByOrgI" +
-      "dRequest\032/.api.v1alpha1.scorecards.ListS" +
-      "corecardsResponse\"V\272\270\221\002\005\n\003\010\310\001\202\323\344\223\002F\"A/ap" +
-      "i/v1alpha1/scorecards/scorecards_support" +
-      "/listscorecardsbyorgid:\001*B\276\001\n)com.tcn.cl" +
-      "oud.api.api.v1alpha1.scorecardsB\023Support" +
-      "ServiceProtoP\001\242\002\003AVS\252\002\027Api.V1alpha1.Scor" +
-      "ecards\312\002\027Api\\V1alpha1\\Scorecards\342\002#Api\\V" +
-      "1alpha1\\Scorecards\\GPBMetadata\352\002\031Api::V1" +
-      "alpha1::Scorecardsb\006proto3"
+      "ionsResponse\"V\272\270\221\002\005\n\003\010\310\001\202\323\344\223\002F\"A/api/v1a" +
+      "lpha1/scorecards/scorecardssupport/liste" +
+      "valuationsbyorgid:\001*\022\352\001\n\032ListAutoEvaluat" +
+      "ionsByOrgId\022:.api.v1alpha1.scorecards.Li" +
+      "stAutoEvaluationsByOrgIdRequest\0324.api.v1" +
+      "alpha1.scorecards.ListAutoEvaluationsRes" +
+      "ponse\"Z\272\270\221\002\005\n\003\010\310\001\202\323\344\223\002J\"E/api/v1alpha1/s" +
+      "corecards/scorecardssupport/listautoeval" +
+      "uationsbyorgid:\001*\022\336\001\n\027DeleteEvaluationBy" +
+      "OrgId\0227.api.v1alpha1.scorecards.DeleteEv" +
+      "aluationByOrgIdRequest\0321.api.v1alpha1.sc" +
+      "orecards.DeleteEvaluationResponse\"W\272\270\221\002\005" +
+      "\n\003\010\310\001\202\323\344\223\002G\"B/api/v1alpha1/scorecards/sc" +
+      "orecardssupport/deleteevaluationbyorgid:" +
+      "\001*\022\356\001\n\033DeleteAutoEvaluationByOrgId\022;.api" +
+      ".v1alpha1.scorecards.DeleteAutoEvaluatio" +
+      "nByOrgIdRequest\0325.api.v1alpha1.scorecard" +
+      "s.DeleteAutoEvaluationResponse\"[\272\270\221\002\005\n\003\010" +
+      "\310\001\202\323\344\223\002K\"F/api/v1alpha1/scorecards/score" +
+      "cardssupport/deleteautoevaluationbyorgid" +
+      ":\001*\022\326\001\n\025ListScorecardsByOrgId\0225.api.v1al" +
+      "pha1.scorecards.ListScorecardsByOrgIdReq" +
+      "uest\032/.api.v1alpha1.scorecards.ListScore" +
+      "cardsResponse\"U\272\270\221\002\005\n\003\010\310\001\202\323\344\223\002E\"@/api/v1" +
+      "alpha1/scorecards/scorecardssupport/list" +
+      "scorecardsbyorgid:\001*B\276\001\n)com.tcn.cloud.a" +
+      "pi.api.v1alpha1.scorecardsB\023SupportServi" +
+      "ceProtoP\001\242\002\003AVS\252\002\027Api.V1alpha1.Scorecard" +
+      "s\312\002\027Api\\V1alpha1\\Scorecards\342\002#Api\\V1alph" +
+      "a1\\Scorecards\\GPBMetadata\352\002\031Api::V1alpha" +
+      "1::Scorecardsb\006proto3"
     };
     descriptor = com.google.protobuf.Descriptors.FileDescriptor
       .internalBuildGeneratedFileFrom(descriptorData,
