e7e22c271b4554988c5c9be8c9c8d9ee3f0d6e6b
TCN CI <ci@tcnp3.com>
2024-04-29 17:01:23 +0000

    update tcnapi-jdk8 > Merge branch '499-add-scorecards-permission-to-some-wfm-endpoints' into 'master'
    Resolve "Add scorecards permission to some WFM endpoints"
    
    Closes #499
    
    See merge request tcnpublic/api!522

--- src/main/java/com/tcn/cloud/api/api/v0alpha/HeaderGroupOrBuilder.java
+++ src/main/java/com/tcn/cloud/api/api/v0alpha/HeaderGroupOrBuilder.java
@@ -28,79 +28,43 @@ public interface HeaderGroupOrBuilder extends
       getNameBytes();
 
   /**
-   * <code>repeated string headers = 2 [json_name = "headers", deprecated = true];</code>
-   * @deprecated api.v0alpha.HeaderGroup.headers is deprecated.
-   *     See api/v0alpha/omniapi.proto;l=1604
+   * <pre>
+   * the headers provided by this group
+   * </pre>
+   *
+   * <code>repeated string headers = 2 [json_name = "headers"];</code>
    * @return A list containing the headers.
    */
-  @java.lang.Deprecated java.util.List<java.lang.String>
+  java.util.List<java.lang.String>
       getHeadersList();
   /**
-   * <code>repeated string headers = 2 [json_name = "headers", deprecated = true];</code>
-   * @deprecated api.v0alpha.HeaderGroup.headers is deprecated.
-   *     See api/v0alpha/omniapi.proto;l=1604
+   * <pre>
+   * the headers provided by this group
+   * </pre>
+   *
+   * <code>repeated string headers = 2 [json_name = "headers"];</code>
    * @return The count of headers.
    */
-  @java.lang.Deprecated int getHeadersCount();
+  int getHeadersCount();
   /**
-   * <code>repeated string headers = 2 [json_name = "headers", deprecated = true];</code>
-   * @deprecated api.v0alpha.HeaderGroup.headers is deprecated.
-   *     See api/v0alpha/omniapi.proto;l=1604
+   * <pre>
+   * the headers provided by this group
+   * </pre>
+   *
+   * <code>repeated string headers = 2 [json_name = "headers"];</code>
    * @param index The index of the element to return.
    * @return The headers at the given index.
    */
-  @java.lang.Deprecated java.lang.String getHeaders(int index);
+  java.lang.String getHeaders(int index);
   /**
-   * <code>repeated string headers = 2 [json_name = "headers", deprecated = true];</code>
-   * @deprecated api.v0alpha.HeaderGroup.headers is deprecated.
-   *     See api/v0alpha/omniapi.proto;l=1604
+   * <pre>
+   * the headers provided by this group
+   * </pre>
+   *
+   * <code>repeated string headers = 2 [json_name = "headers"];</code>
    * @param index The index of the value to return.
    * @return The bytes of the headers at the given index.
    */
-  @java.lang.Deprecated com.google.protobuf.ByteString
+  com.google.protobuf.ByteString
       getHeadersBytes(int index);
-
-  /**
-   * <pre>
-   * the headers provided by this group
-   * </pre>
-   *
-   * <code>repeated .api.v0alpha.Header list_headers = 3 [json_name = "listHeaders"];</code>
-   */
-  java.util.List<com.tcn.cloud.api.api.v0alpha.Header> 
-      getListHeadersList();
-  /**
-   * <pre>
-   * the headers provided by this group
-   * </pre>
-   *
-   * <code>repeated .api.v0alpha.Header list_headers = 3 [json_name = "listHeaders"];</code>
-   */
-  com.tcn.cloud.api.api.v0alpha.Header getListHeaders(int index);
-  /**
-   * <pre>
-   * the headers provided by this group
-   * </pre>
-   *
-   * <code>repeated .api.v0alpha.Header list_headers = 3 [json_name = "listHeaders"];</code>
-   */
-  int getListHeadersCount();
-  /**
-   * <pre>
-   * the headers provided by this group
-   * </pre>
-   *
-   * <code>repeated .api.v0alpha.Header list_headers = 3 [json_name = "listHeaders"];</code>
-   */
-  java.util.List<? extends com.tcn.cloud.api.api.v0alpha.HeaderOrBuilder> 
-      getListHeadersOrBuilderList();
-  /**
-   * <pre>
-   * the headers provided by this group
-   * </pre>
-   *
-   * <code>repeated .api.v0alpha.Header list_headers = 3 [json_name = "listHeaders"];</code>
-   */
-  com.tcn.cloud.api.api.v0alpha.HeaderOrBuilder getListHeadersOrBuilder(
-      int index);
 }
