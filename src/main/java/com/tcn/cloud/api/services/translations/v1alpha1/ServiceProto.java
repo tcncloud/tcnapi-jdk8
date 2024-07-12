0fd1783931ce2e1368eab059bded11f6e1b14b39
TCN CI <ci@tcnp3.com>
2024-07-12 23:39:23 +0000

    update tcnapi-jdk8 > list languages call

--- src/main/java/com/tcn/cloud/api/services/translations/v1alpha1/ServiceProto.java
+++ src/main/java/com/tcn/cloud/api/services/translations/v1alpha1/ServiceProto.java
@@ -27,7 +27,7 @@ public final class ServiceProto {
       ".proto\022\036services.translations.v1alpha1\032\027" +
       "annotations/authz.proto\032\034google/api/anno" +
       "tations.proto\032-services/translations/v1a" +
-      "lpha1/entities.proto2\302\r\n\023TranslationsSer" +
+      "lpha1/entities.proto2\205\017\n\023TranslationsSer" +
       "vice\022\315\001\n\021TranslateTemplate\0228.services.tr" +
       "anslations.v1alpha1.TranslateTemplateReq" +
       "uest\0329.services.translations.v1alpha1.Tr" +
@@ -38,45 +38,50 @@ public final class ServiceProto {
       "onsRequest\0328.services.translations.v1alp" +
       "ha1.ListTranslationsResponse\"E\272\270\221\002\005\n\003\010\310\001" +
       "\202\323\344\223\0025\"0/services/translations/v1alpha1/" +
-      "listtranslations:\001*\022\320\001\n\021UpdateTranslatio" +
-      "n\0228.services.translations.v1alpha1.Updat" +
-      "eTranslationRequest\0329.services.translati" +
-      "ons.v1alpha1.UpdateTranslationResponse\"F" +
-      "\272\270\221\002\005\n\003\010\310\001\202\323\344\223\0026\"1/services/translations" +
-      "/v1alpha1/updatetranslation:\001*\022\340\001\n\025Trigg" +
-      "erLLMTranslation\022<.services.translations" +
-      ".v1alpha1.TriggerLLMTranslationRequest\032=" +
-      ".services.translations.v1alpha1.TriggerL" +
-      "LMTranslationResponse\"J\272\270\221\002\005\n\003\010\310\001\202\323\344\223\002:\"" +
-      "5/services/translations/v1alpha1/trigger" +
-      "llmtranslation:\001*\022\344\001\n\026TriggerLLMTranslat" +
-      "ions\022=.services.translations.v1alpha1.Tr" +
-      "iggerLLMTranslationsRequest\032>.services.t" +
-      "ranslations.v1alpha1.TriggerLLMTranslati" +
-      "onsResponse\"K\272\270\221\002\005\n\003\010\310\001\202\323\344\223\002;\"6/services" +
-      "/translations/v1alpha1/triggerllmtransla" +
-      "tions:\001*\022\314\001\n\020SetSystemMessage\0227.services" +
-      ".translations.v1alpha1.SetSystemMessageR" +
-      "equest\0328.services.translations.v1alpha1." +
-      "SetSystemMessageResponse\"E\272\270\221\002\005\n\003\010\310\001\202\323\344\223" +
-      "\0025\"0/services/translations/v1alpha1/sets" +
-      "ystemmessage:\001*\022\314\001\n\020GetSystemMessage\0227.s" +
-      "ervices.translations.v1alpha1.GetSystemM" +
-      "essageRequest\0328.services.translations.v1" +
-      "alpha1.GetSystemMessageResponse\"E\272\270\221\002\005\n\003" +
-      "\010\310\001\202\323\344\223\0025\"0/services/translations/v1alph" +
-      "a1/getsystemmessage:\001*\022\320\001\n\021TestSystemMes" +
-      "sage\0228.services.translations.v1alpha1.Te" +
-      "stSystemMessageRequest\0329.services.transl" +
-      "ations.v1alpha1.TestSystemMessageRespons" +
-      "e\"F\272\270\221\002\005\n\003\010\310\001\202\323\344\223\0026\"1/services/translati" +
-      "ons/v1alpha1/testsystemmessage:\001*B\332\001\n0co" +
-      "m.tcn.cloud.api.services.translations.v1" +
-      "alpha1B\014ServiceProtoP\001\242\002\003STX\252\002\036Services." +
-      "Translations.V1alpha1\312\002\036Services\\Transla" +
-      "tions\\V1alpha1\342\002*Services\\Translations\\V" +
-      "1alpha1\\GPBMetadata\352\002 Services::Translat" +
-      "ions::V1alpha1b\006proto3"
+      "listtranslations:\001*\022\300\001\n\rListLanguages\0224." +
+      "services.translations.v1alpha1.ListLangu" +
+      "agesRequest\0325.services.translations.v1al" +
+      "pha1.ListLanguagesResponse\"B\272\270\221\002\005\n\003\010\310\001\202\323" +
+      "\344\223\0022\"-/services/translations/v1alpha1/li" +
+      "stlanguages:\001*\022\320\001\n\021UpdateTranslation\0228.s" +
+      "ervices.translations.v1alpha1.UpdateTran" +
+      "slationRequest\0329.services.translations.v" +
+      "1alpha1.UpdateTranslationResponse\"F\272\270\221\002\005" +
+      "\n\003\010\310\001\202\323\344\223\0026\"1/services/translations/v1al" +
+      "pha1/updatetranslation:\001*\022\340\001\n\025TriggerLLM" +
+      "Translation\022<.services.translations.v1al" +
+      "pha1.TriggerLLMTranslationRequest\032=.serv" +
+      "ices.translations.v1alpha1.TriggerLLMTra" +
+      "nslationResponse\"J\272\270\221\002\005\n\003\010\310\001\202\323\344\223\002:\"5/ser" +
+      "vices/translations/v1alpha1/triggerllmtr" +
+      "anslation:\001*\022\344\001\n\026TriggerLLMTranslations\022" +
+      "=.services.translations.v1alpha1.Trigger" +
+      "LLMTranslationsRequest\032>.services.transl" +
+      "ations.v1alpha1.TriggerLLMTranslationsRe" +
+      "sponse\"K\272\270\221\002\005\n\003\010\310\001\202\323\344\223\002;\"6/services/tran" +
+      "slations/v1alpha1/triggerllmtranslations" +
+      ":\001*\022\314\001\n\020SetSystemMessage\0227.services.tran" +
+      "slations.v1alpha1.SetSystemMessageReques" +
+      "t\0328.services.translations.v1alpha1.SetSy" +
+      "stemMessageResponse\"E\272\270\221\002\005\n\003\010\310\001\202\323\344\223\0025\"0/" +
+      "services/translations/v1alpha1/setsystem" +
+      "message:\001*\022\314\001\n\020GetSystemMessage\0227.servic" +
+      "es.translations.v1alpha1.GetSystemMessag" +
+      "eRequest\0328.services.translations.v1alpha" +
+      "1.GetSystemMessageResponse\"E\272\270\221\002\005\n\003\010\310\001\202\323" +
+      "\344\223\0025\"0/services/translations/v1alpha1/ge" +
+      "tsystemmessage:\001*\022\320\001\n\021TestSystemMessage\022" +
+      "8.services.translations.v1alpha1.TestSys" +
+      "temMessageRequest\0329.services.translation" +
+      "s.v1alpha1.TestSystemMessageResponse\"F\272\270" +
+      "\221\002\005\n\003\010\310\001\202\323\344\223\0026\"1/services/translations/v" +
+      "1alpha1/testsystemmessage:\001*B\332\001\n0com.tcn" +
+      ".cloud.api.services.translations.v1alpha" +
+      "1B\014ServiceProtoP\001\242\002\003STX\252\002\036Services.Trans" +
+      "lations.V1alpha1\312\002\036Services\\Translations" +
+      "\\V1alpha1\342\002*Services\\Translations\\V1alph" +
+      "a1\\GPBMetadata\352\002 Services::Translations:" +
+      ":V1alpha1b\006proto3"
     };
     descriptor = com.google.protobuf.Descriptors.FileDescriptor
       .internalBuildGeneratedFileFrom(descriptorData,
