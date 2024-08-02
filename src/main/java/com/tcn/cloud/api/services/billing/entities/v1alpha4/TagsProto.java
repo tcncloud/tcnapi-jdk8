daf8570bbc2fd633f0e49eed4324a43ed7c688b3
TCN CI <ci@tcnp3.com>
2024-08-02 16:58:32 +0000

    update tcnapi-jdk8 > Added correct label for data mangement compliance

--- src/main/java/com/tcn/cloud/api/services/billing/entities/v1alpha4/TagsProto.java
+++ src/main/java/com/tcn/cloud/api/services/billing/entities/v1alpha4/TagsProto.java
@@ -40,20 +40,20 @@ public final class TagsProto {
       "R\ndeleteTime\022\036\n\010category\030\006 \001(\tB\002\030\001R\010cate" +
       "gory\022W\n\020billing_category\030\007 \001(\0162,.service" +
       "s.billing.entities.v1alpha4.CategoryR\017bi" +
-      "llingCategory*\227\002\n\010Category\022\030\n\024CATEGORY_U" +
+      "llingCategory*\242\002\n\010Category\022\030\n\024CATEGORY_U" +
       "NSPECIFIED\020\000\022%\n!CATEGORY_COMMUNICATIONS_" +
       "OMNI_CHAT\020d\022&\n\"CATEGORY_COMMUNICATIONS_O" +
       "MNI_EMAIL\020e\022$\n CATEGORY_COMMUNICATIONS_O" +
       "MNI_SMS\020f\022&\n\"CATEGORY_COMMUNICATIONS_OMN" +
       "I_AGENT\020g\022*\n&CATEGORY_COMMUNICATIONS_OMN" +
-      "I_RESOURCES\020h\022(\n#CATEGORY_DATA_MANAGEMEN" +
-      "T_COMPLIANCE\020\310\001B\354\001\n4com.tcn.cloud.api.se" +
-      "rvices.billing.entities.v1alpha4B\tTagsPr" +
-      "otoP\001\242\002\003SBE\252\002\"Services.Billing.Entities." +
-      "V1alpha4\312\002\"Services\\Billing\\Entities\\V1a" +
-      "lpha4\342\002.Services\\Billing\\Entities\\V1alph" +
-      "a4\\GPBMetadata\352\002%Services::Billing::Enti" +
-      "ties::V1alpha4b\006proto3"
+      "I_RESOURCES\020h\0223\n.CATEGORY_DATA_MANAGEMEN" +
+      "T_COMPLIANCE_COMPLIANCE\020\310\001B\354\001\n4com.tcn.c" +
+      "loud.api.services.billing.entities.v1alp" +
+      "ha4B\tTagsProtoP\001\242\002\003SBE\252\002\"Services.Billin" +
+      "g.Entities.V1alpha4\312\002\"Services\\Billing\\E" +
+      "ntities\\V1alpha4\342\002.Services\\Billing\\Enti" +
+      "ties\\V1alpha4\\GPBMetadata\352\002%Services::Bi" +
+      "lling::Entities::V1alpha4b\006proto3"
     };
     descriptor = com.google.protobuf.Descriptors.FileDescriptor
       .internalBuildGeneratedFileFrom(descriptorData,
