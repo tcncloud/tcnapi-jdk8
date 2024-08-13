f7fd5b5ca26719860ef9e4824fb39fb57e744df0
TCN CI <ci@tcnp3.com>
2024-08-13 21:09:36 +0000

    update tcnapi-jdk8 > Return the id of the newly created exile certificate

--- src/main/java/com/tcn/cloud/api/services/org/exile_certificate_manager/v1alpha1/CreateExileCertificateResponseOrBuilder.java
+++ src/main/java/com/tcn/cloud/api/services/org/exile_certificate_manager/v1alpha1/CreateExileCertificateResponseOrBuilder.java
@@ -26,4 +26,24 @@ public interface CreateExileCertificateResponseOrBuilder extends
    */
   com.google.protobuf.ByteString
       getEncodedExileCertificateBytes();
+
+  /**
+   * <pre>
+   * The id of the exile certificate that was created.
+   * </pre>
+   *
+   * <code>string exile_certificate_id = 2 [json_name = "exileCertificateId"];</code>
+   * @return The exileCertificateId.
+   */
+  java.lang.String getExileCertificateId();
+  /**
+   * <pre>
+   * The id of the exile certificate that was created.
+   * </pre>
+   *
+   * <code>string exile_certificate_id = 2 [json_name = "exileCertificateId"];</code>
+   * @return The bytes for exileCertificateId.
+   */
+  com.google.protobuf.ByteString
+      getExileCertificateIdBytes();
 }
