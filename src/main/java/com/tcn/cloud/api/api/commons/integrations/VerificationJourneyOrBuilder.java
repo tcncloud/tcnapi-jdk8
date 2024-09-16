e8a46fbe2b41b6e755a9bd036260faadf5e65094
TCN CI <ci@tcnp3.com>
2024-09-16 16:09:07 +0000

    update tcnapi-jdk8 > Add GetUserAuthentication entpoint

--- src/main/java/com/tcn/cloud/api/api/commons/integrations/VerificationJourneyOrBuilder.java
+++ src/main/java/com/tcn/cloud/api/api/commons/integrations/VerificationJourneyOrBuilder.java
@@ -15,7 +15,7 @@ public interface VerificationJourneyOrBuilder extends
    *
    * <code>string date_of_birth_field = 1 [json_name = "dateOfBirthField", deprecated = true];</code>
    * @deprecated api.commons.integrations.VerificationJourney.date_of_birth_field is deprecated.
-   *     See api/commons/integrations/integrations.proto;l=1101
+   *     See api/commons/integrations/integrations.proto;l=1102
    * @return The dateOfBirthField.
    */
   @java.lang.Deprecated java.lang.String getDateOfBirthField();
@@ -27,7 +27,7 @@ public interface VerificationJourneyOrBuilder extends
    *
    * <code>string date_of_birth_field = 1 [json_name = "dateOfBirthField", deprecated = true];</code>
    * @deprecated api.commons.integrations.VerificationJourney.date_of_birth_field is deprecated.
-   *     See api/commons/integrations/integrations.proto;l=1101
+   *     See api/commons/integrations/integrations.proto;l=1102
    * @return The bytes for dateOfBirthField.
    */
   @java.lang.Deprecated com.google.protobuf.ByteString
@@ -41,7 +41,7 @@ public interface VerificationJourneyOrBuilder extends
    *
    * <code>string last_name_field = 2 [json_name = "lastNameField", deprecated = true];</code>
    * @deprecated api.commons.integrations.VerificationJourney.last_name_field is deprecated.
-   *     See api/commons/integrations/integrations.proto;l=1104
+   *     See api/commons/integrations/integrations.proto;l=1105
    * @return The lastNameField.
    */
   @java.lang.Deprecated java.lang.String getLastNameField();
@@ -53,7 +53,7 @@ public interface VerificationJourneyOrBuilder extends
    *
    * <code>string last_name_field = 2 [json_name = "lastNameField", deprecated = true];</code>
    * @deprecated api.commons.integrations.VerificationJourney.last_name_field is deprecated.
-   *     See api/commons/integrations/integrations.proto;l=1104
+   *     See api/commons/integrations/integrations.proto;l=1105
    * @return The bytes for lastNameField.
    */
   @java.lang.Deprecated com.google.protobuf.ByteString
@@ -67,7 +67,7 @@ public interface VerificationJourneyOrBuilder extends
    *
    * <code>string first_name_field = 3 [json_name = "firstNameField", deprecated = true];</code>
    * @deprecated api.commons.integrations.VerificationJourney.first_name_field is deprecated.
-   *     See api/commons/integrations/integrations.proto;l=1107
+   *     See api/commons/integrations/integrations.proto;l=1108
    * @return The firstNameField.
    */
   @java.lang.Deprecated java.lang.String getFirstNameField();
@@ -79,7 +79,7 @@ public interface VerificationJourneyOrBuilder extends
    *
    * <code>string first_name_field = 3 [json_name = "firstNameField", deprecated = true];</code>
    * @deprecated api.commons.integrations.VerificationJourney.first_name_field is deprecated.
-   *     See api/commons/integrations/integrations.proto;l=1107
+   *     See api/commons/integrations/integrations.proto;l=1108
    * @return The bytes for firstNameField.
    */
   @java.lang.Deprecated com.google.protobuf.ByteString
@@ -93,7 +93,7 @@ public interface VerificationJourneyOrBuilder extends
    *
    * <code>string zip_field = 4 [json_name = "zipField", deprecated = true];</code>
    * @deprecated api.commons.integrations.VerificationJourney.zip_field is deprecated.
-   *     See api/commons/integrations/integrations.proto;l=1110
+   *     See api/commons/integrations/integrations.proto;l=1111
    * @return The zipField.
    */
   @java.lang.Deprecated java.lang.String getZipField();
@@ -105,7 +105,7 @@ public interface VerificationJourneyOrBuilder extends
    *
    * <code>string zip_field = 4 [json_name = "zipField", deprecated = true];</code>
    * @deprecated api.commons.integrations.VerificationJourney.zip_field is deprecated.
-   *     See api/commons/integrations/integrations.proto;l=1110
+   *     See api/commons/integrations/integrations.proto;l=1111
    * @return The bytes for zipField.
    */
   @java.lang.Deprecated com.google.protobuf.ByteString
@@ -119,7 +119,7 @@ public interface VerificationJourneyOrBuilder extends
    *
    * <code>string last4_ssn = 5 [json_name = "last4Ssn", deprecated = true];</code>
    * @deprecated api.commons.integrations.VerificationJourney.last4_ssn is deprecated.
-   *     See api/commons/integrations/integrations.proto;l=1113
+   *     See api/commons/integrations/integrations.proto;l=1114
    * @return The last4Ssn.
    */
   @java.lang.Deprecated java.lang.String getLast4Ssn();
@@ -131,7 +131,7 @@ public interface VerificationJourneyOrBuilder extends
    *
    * <code>string last4_ssn = 5 [json_name = "last4Ssn", deprecated = true];</code>
    * @deprecated api.commons.integrations.VerificationJourney.last4_ssn is deprecated.
-   *     See api/commons/integrations/integrations.proto;l=1113
+   *     See api/commons/integrations/integrations.proto;l=1114
    * @return The bytes for last4Ssn.
    */
   @java.lang.Deprecated com.google.protobuf.ByteString
