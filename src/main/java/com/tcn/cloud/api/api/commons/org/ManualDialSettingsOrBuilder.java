265445a144375aa52c63b6a661828af027e4469e
TCN CI <ci@tcnp3.com>
2024-12-11 22:06:34 +0000

    update tcnapi-jdk8 > TCN Public API #801.  Add the template property sid to the details proto.

--- src/main/java/com/tcn/cloud/api/api/commons/org/ManualDialSettingsOrBuilder.java
+++ src/main/java/com/tcn/cloud/api/api/commons/org/ManualDialSettingsOrBuilder.java
@@ -511,7 +511,7 @@ public interface ManualDialSettingsOrBuilder extends
    *
    * <code>bool enable_reject_option_for_approvers = 29 [json_name = "enableRejectOptionForApprovers", deprecated = true];</code>
    * @deprecated api.commons.org.ManualDialSettings.enable_reject_option_for_approvers is deprecated.
-   *     See api/commons/org/huntgroup.proto;l=587
+   *     See api/commons/org/huntgroup.proto;l=589
    * @return The enableRejectOptionForApprovers.
    */
   @java.lang.Deprecated boolean getEnableRejectOptionForApprovers();
